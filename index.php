8b6854a5d1e955e30109b6d2897a9ae2342bcbb1
GEAN PEREIRA DE SOUZA <21276338@CFP127ALUNO.LOCAL>
2022-02-10 16:56:40 -0300

    1.0

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a
+            <img src="img/menu.png" alt="openMenu">
+        </a>
+    </header>
+        <main>
+            <nav>
+                <ul class="menu">
+                    <a href="pages/media.php"><li>Média</li></a>
+                    <a href="pages/calculadora.php"><li>Calculadora</li></a>
+                    <a href="#"><li>Tabuada</li></a>
+                    <a href="#"><li>Pares e impares</li></a>
+                </ul>
+            </nav>
+        </main>
+    
+</body>
+</html>
---
+++ pages/mudoloMedia/config.php
@@ -0,0 +1,12 @@
+<?php
+    /*******************************************************************************************
+     * Objetivo: Arquivo responsavel por realizar e padronizar todas as variaveis e constantes 
+     *      que serão utilizado em todo o projeto
+     * Autor: Gean
+     * Data: 04/02/2022
+     * Versão: 0.1
+     *******************************************************************************************/
+    //Constantes do projeto
+    define('ERRO_PREENCHER', '<script> alert("É obrigatório preencher todos os valores acima!");</script>');
+    define('ERRO_APENAS_NUMEROS', '<script> alert(" Apenas Numeros "); </script>');
+?>
